db3c47ab0f24c40e7c36d9117695a8cbaac0b8e0
Renal Diansyah <renaldiansyah16@gmail.com>
2024-09-09 15:32:33 +0700

    perubahan wp-config

--- wp-config.php
+++ wp-config.php
@@ -37,6 +37,9 @@ define( 'DB_CHARSET', 'utf8' );
 /** The database collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
 
+define('WP_HOME', 'http://localhost/Wordpress_KomporGAS');
+define('WP_SITEURL', 'http://localhost/Wordpress_KomporGAS');
+
 /**#@+
  * Authentication unique keys and salts.
  *
